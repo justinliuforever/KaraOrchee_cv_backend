50cd506e07926d0d7215817d03c3dcc4d85d3b54
Justin Liu <justinliu@ark7.com>
2024-09-25 21:08:25 -0400

    adjust code format

--- app/eye_state_detector.py
+++ app/eye_state_detector.py
@@ -6,7 +6,7 @@ import time
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 
-def detect_eye_state(frame):
+def detect_eye_state(frame, show_landmarks=True):
     # Configuration parameters
     EAR_THRESHOLD = 0.15  # Adjusted threshold for determining if eyes are open or closed
 
@@ -54,12 +54,13 @@ def detect_eye_state(frame):
         # Detect and process face landmarks
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=frame_bgr,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
+                if show_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image=frame_bgr,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                        connection_drawing_spec=mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
                 
                 # Calculate EAR for both eyes
                 ear = calculate_eye_aspect_ratio(face_landmarks.landmark)
--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, UploadFile, File, WebSocket, WebSocketDisconnect
 from fastapi.responses import JSONResponse
 import cv2
 import numpy as np
-from .cv_algorithm import process_frame
-from .blink_detection import detect_eye_state  # Import the new eye state detection function
+from .mouth_state_detector import process_frame
+from .eye_state_detector import detect_eye_state  # Import the new eye state detection function
 from starlette.websockets import WebSocketState
+import json
 
 app = FastAPI()
 
@@ -16,8 +17,8 @@ async def process_image(file: UploadFile = File(...)):
     status, _ = process_frame(img)
     return JSONResponse(content={"status": status})
 
-@app.websocket("/ws/video")
-async def websocket_endpoint(websocket: WebSocket):
+@app.websocket("/ws/mouth-state")
+async def mouth_state_websocket(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
@@ -27,29 +28,32 @@ async def websocket_endpoint(websocket: WebSocket):
             status, frame_encoded = process_frame(frame)
             await websocket.send_json({"status": status, "frame": frame_encoded.hex()})
     except WebSocketDisconnect:
-        print("WebSocket connection closed")
+        print("Mouth state WebSocket connection closed")
     except Exception as e:
         print(f"Connection closed with error: {e}")
     finally:
         if websocket.client_state == WebSocketState.CONNECTED:
             await websocket.close()
 
-@app.websocket("/ws/eye-state-detection")
-async def eye_state_detection_endpoint(websocket: WebSocket):
+@app.websocket("/ws/eye-state")
+async def eye_state_websocket(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            data = await websocket.receive_bytes()
-            nparr = np.frombuffer(data, np.uint8)
+            data = await websocket.receive_text()
+            parsed_data = json.loads(data)
+            frame_data = parsed_data['frame']
+            show_landmarks = parsed_data['showLandmarks']
+            
+            nparr = np.array(frame_data, dtype=np.uint8)
             frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
             
-            # Call the eye state detection function
-            status, processed_frame = detect_eye_state(frame)
-            _, frame_encoded = cv2.imencode('.jpg', processed_frame)  # Encode the processed frame
+            status, processed_frame = detect_eye_state(frame, show_landmarks)
+            _, frame_encoded = cv2.imencode('.jpg', processed_frame)
             
             await websocket.send_json({"status": status, "frame": frame_encoded.tobytes().hex()})
     except WebSocketDisconnect:
-        print("Eye state detection WebSocket connection closed")
+        print("Eye state WebSocket connection closed")
     except Exception as e:
         print(f"Connection closed with error: {e}")
     finally:
